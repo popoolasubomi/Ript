96c03981b45a6b480d0b001bfd6979c3619b1018
Ogooluwasubomi Ayotomi Popoola <subomi@fb.com>
2021-05-27 09:40:10 -0400

    Initial Commit

---
+++ ripped/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ripped
+//
+//  Created by Ogooluwasubomi Ayotomi Popoola on 5/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
